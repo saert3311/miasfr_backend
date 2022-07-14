d8c646a9e255f7a5b1c45ecee8b3bc70d324a12b
Jose Duarte <saert@outlook.com>
2022-07-14 16:35:24 -0400

    settings

--- miasfr_backend/settings.py
+++ miasfr_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = str(os.getenv('MIASFR_SECRET'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['orto.miascaffold.com', 'localhost']
 
 
 # Application definition
