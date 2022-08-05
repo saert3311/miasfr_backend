627c851fb19760fb0fdb8afe6b3143ba4cb3db83
Jose Duarte <saert@outlook.com>
2022-08-05 12:23:53 -0400

    Item Listing

--- miasfr_backend/settings.py
+++ miasfr_backend/settings.py
@@ -56,9 +56,9 @@ CORS_ALLOWED_ORIGINS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
